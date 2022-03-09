2b2ed30294ff9bcd62d38905c5243a29560a0de0
talama <talama@protonmail.com>
2022-03-09 15:09:50 +0100

    Day 2 solution 2

--- 2/2.js
+++ 2/2.js
@@ -1,13 +1,13 @@
 const fs = require('fs').promises;
 
-const test = [
-  { direction: 'forward', value: 5 },
-  { direction: 'down', value: 5 },
-  { direction: 'forward', value: 8 },
-  { direction: 'up', value: 3 },
-  { direction: 'down', value: 8 },
-  { direction: 'forward', value: 2 },
-];
+// const test = [
+//   { direction: 'forward', value: 5 },
+//   { direction: 'down', value: 5 },
+//   { direction: 'forward', value: 8 },
+//   { direction: 'up', value: 3 },
+//   { direction: 'down', value: 8 },
+//   { direction: 'forward', value: 2 },
+// ];
 
 /**
  * Formats the input file as an array of objects.
@@ -57,5 +57,40 @@ function solution1(data) {
   return finalPosition.horizontal * finalPosition.depth;
 }
 
-console.log(solution1(test));
-format('./input.txt').then(solution1).then(console.log);
+/**
+ * Updates the current pos ition based on the directions in the data array.
+ * es. {direction: 'up', value: 5}
+ * @param {Array} data
+ * @returns {Number} - product of final position and final depth.
+ */
+function solution2(data) {
+  const finalPosition = data.reduce(
+    (position, instruction) => {
+      const newPosition = { ...position };
+      switch (instruction.direction) {
+        case 'forward':
+          newPosition.horizontal += instruction.value;
+          newPosition.depth += newPosition.aim * instruction.value;
+          break;
+        case 'up':
+          newPosition.aim -= instruction.value;
+          break;
+        case 'down':
+          newPosition.aim += instruction.value;
+          break;
+        default:
+          break;
+      }
+      return newPosition;
+    },
+    { horizontal: 0, depth: 0, aim: 0 },
+  );
+  return finalPosition.horizontal * finalPosition.depth;
+}
+
+// console.log(solution1(test));
+// console.log(solution2(test));
+format('./input.txt').then((data) => {
+  console.log(solution1(data));
+  console.log(solution2(data));
+});
