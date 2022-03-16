bd5ee4f9cc74d5364699a56950ea48dfba659ce8
talama <talama@protonmail.com>
2022-03-16 17:37:26 +0100

    Day5 Solution 1 first implementation

--- 5/5.js
+++ 5/5.js
@@ -2,7 +2,7 @@ const fs = require('fs').promises;
 
 async function format(path) {
   const data = await fs.readFile(path, 'utf-8');
-  return data
+  const lines = data
     .trim()
     .split('\n')
     .map((str) => {
@@ -13,9 +13,72 @@ async function format(path) {
         x2: +line[2],
         y2: +line[3],
       };
-    });
+    })
+    .filter((line) => line.x1 === line.x2 || line.y1 === line.y2);
+
+  const gridSize = lines.reduce(
+    (result, line) => {
+      const grid = { ...result };
+      if (line.x1 > grid.x) grid.x = line.x1 + 1;
+      if (line.x2 > grid.x) grid.x = line.x2 + 1;
+      if (line.y1 > grid.y) grid.y = line.y1 + 1;
+      if (line.y2 > grid.y) grid.y = line.y2 + 1;
+      return grid;
+    },
+    { x: 0, y: 0 },
+  );
+  return [lines, gridSize];
 }
 
-format('./test.txt').then((lines) => {
-  console.log(lines);
+function printGrid(grid) {
+  grid.forEach((row) => {
+    let rowString = '';
+    row.forEach((point) => {
+      rowString += `${point}`;
+    });
+    console.log(rowString);
+  });
+}
+
+function drawLine(line, grid) {
+  const localGrid = [...grid];
+  const isHorizontal = line.y1 === line.y2;
+  if (isHorizontal) {
+    const y = line.y1;
+    const [xStart, xEnd] =
+      line.x1 <= line.x2 ? [line.x1, line.x2] : [line.x2, line.x1];
+    localGrid[y] = localGrid[y].map((point, index) => {
+      if (index >= xStart && index <= xEnd) {
+        return point + 1;
+      }
+      return point;
+    });
+  } else {
+    const x = line.x1;
+    const [yStart, yEnd] =
+      line.y1 <= line.y2 ? [line.y1, line.y2] : [line.y2, line.y1];
+    localGrid.forEach((row, rowIndex) => {
+      if (rowIndex >= yStart && rowIndex <= yEnd)
+        localGrid[rowIndex][x] += 1;
+    });
+  }
+  return localGrid;
+}
+
+format('./input.txt').then(([lines, gridSize]) => {
+  let grid = Array(gridSize.y)
+    .fill(0)
+    .map(() => Array(gridSize.x).fill(0));
+
+  lines.forEach((line) => {
+    grid = drawLine(line, grid);
+  });
+  const overlaps = grid.reduce((result, row) => {
+    let current = result;
+    row.forEach((point) => {
+      if (point >= 2) current += 1;
+    });
+    return current;
+  }, 0);
+  console.log('Solution 1: ', overlaps);
 });
--- 5/test.js
+++
@@ -1,3 +0,0 @@
-const str = '01,92 -> 523,1999';
-
-console.log(str.match(/\d{1,3}/g));
