19de6a278b70d92b370f12e086b257217e873a03
talama <talama@protonmail.com>
2022-04-04 12:17:38 +0200

    Day 6 solution 2

--- 6/6.js
+++ 6/6.js
@@ -3,7 +3,7 @@ const fs = require('fs').promises;
 /**
  * Format input text file.
  * @param {String} path - file path
- * @returns {Array} - each element of the array is a line in the form [x1, y1, x2, y2]
+ * @returns {Array} - array representing the inital state of the lanternsfish school.
  */
 async function format(path) {
   const data = await fs.readFile(path, 'utf-8');
@@ -13,17 +13,44 @@ async function format(path) {
     .map((num) => +num);
 }
 
-function updateDay(fishArray) {
-  return fishArray.flatMap((fish) => {
-    if (fish === 0) return [6, 8];
-    return fish - 1;
-  });
+/**
+ *
+ * @param {Array} currentState
+ * @returns {Array} - the updated state
+ */
+function updateState(currentState) {
+  // create local copy of current state
+  const localState = currentState;
+  // shift the array to the left and store the number of new fishes to be created.
+  const newFishes = localState.shift();
+  // update the parent position [6] and the new fishes position [8]
+  // and return the updated state.
+  localState[6] += newFishes;
+  localState.push(newFishes);
+  return localState;
 }
 
-format('./input.txt').then((data) => {
-  let result = data;
-  for (let i = 1; i <= 256; i += 1) {
-    result = updateDay(result);
+// Solutions
+format('./input.txt').then((initialState) => {
+  // create an array to store the number of fishes on each timer in the range [0-9]
+  let currentState = Array(9).fill(0);
+  // initialize the array based on the initial state of the fish school.
+  initialState.forEach((elem) => {
+    currentState[elem] += 1;
+  });
+  // update the state of the fish school each day
+  // and calculate the number of fishes for solution1 and solution2
+  for (let day = 1; day <= 256; day += 1) {
+    currentState = updateState(currentState);
+    if (day === 80) {
+      console.log(
+        `Solution 1: ${currentState.reduce(
+          (acc, curr) => acc + curr,
+        )}`,
+      );
+    }
   }
-  console.log(result.length);
+  console.log(
+    `Solution 2: ${currentState.reduce((acc, curr) => acc + curr)}`,
+  );
 });
