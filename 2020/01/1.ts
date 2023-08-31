5885a4ee07a53a5af9f82c9fe9e4c6f1f4f0b441
talama <talama@proton.me>
2023-08-31 23:20:00 +0200

    feat: refactored solution for both parts

--- 2020/01/1.ts
+++ 2020/01/1.ts
@@ -9,43 +9,66 @@ function format(input: string): Array<number> {
     .map((str) => parseInt(str, 10));
 }
 
-function twoSum(list: number[], target: number): void {
-  const len = list.length;
-  const idx: number[] = [];
-  const hash: { [index: number]: number } = {};
+/**
+ * Solves twoSum and threeSum using hashmaps.
+ * @param {string[]} number - Array of numbers to search in.
+ * @param {string[]} target - target number for the sums.
+ */
+function findSums(numList: number[], target: number): void {
+  const len = numList.length;
+  const twoSum: number[] = [];
+  const threeSum: number[] = [];
+  const hashTwo: { [index: number]: number } = {};
   for (let i = 0; i < len; i += 1) {
-    const diff = target - list[i];
-    if (diff in hash) {
-      idx.push(hash[diff], i);
-      break;
-    }
-    hash[list[i]] = i;
-  }
-  console.log(
-    `Two sum: ${list[idx[0]]} * ${list[idx[1]]} = ${
-      list[idx[0]] * list[idx[1]]
-    }`
-  );
-}
-
-function threeSum(list: number[], target: number): void {
-  const len = list.length;
-  const idx: number[] = [];
-  const hash: { [index: number]: number } = {};
-  for (let i = 0; i < len; i += 1) {
-    const diff = target - list[i];
+    const hashThree: { [index: number]: number } = {};
+    const diff = target - numList[i];
+    if (diff in hashTwo) twoSum.push(hashTwo[diff], numList[i]);
+    else hashTwo[numList[i]] = numList[i];
     for (let j = i + 1; j < len; j += 1) {
-      if (diff - list[j] in hash) {
-        idx.push(i, j, hash[diff - list[j]]);
-        break;
-      }
-      hash[list[j]] = j;
+      if (diff - numList[j] in hashThree)
+        threeSum.push(diff - numList[j], numList[j], numList[i]);
+      else hashThree[numList[j]] = numList[j];
     }
   }
-  console.log(`Three sum: ${list[idx[0]]} * ${list[idx[1]]} * ${list[idx[2]]} = ${list[idx[0]] * list[idx[1]] * list[idx[2]]}`)
+  console.log(`Two sum: ${twoSum[0] * twoSum[1]}`);
+  console.log(`Three sum: ${threeSum[0] * threeSum[1] * threeSum[2]}`);
 }
 
-// const test = [1721, 979, 366, 299, 675, 1456];
 const data = format('input.txt');
-twoSum(data, 2020);
-threeSum(data, 2020);
+findSums(data, 2020);
+
+// function twoSum(numList: number[], target: number): void {
+//   const len = numList.length;
+//   const idx: number[] = [];
+//   const hash: { [index: number]: number } = {};
+//   for (let i = 0; i < len; i += 1) {
+//     const diff = target - numList[i];
+//     if (diff in hash) {
+//       idx.push(hash[diff], numList[i]);
+//       break;
+//     }
+//     hash[numList[i]] = numList[i];
+//   }
+//   console.log(`Two sum: ${idx[0] * idx[1]}`);
+// }
+//
+// function threeSum(numList: number[], target: number): void {
+//   const len = numList.length;
+//   const idx: number[] = [];
+//   const hash: { [index: number]: number } = {};
+//   for (let i = 0; i < len; i += 1) {
+//     const diff = target - numList[i];
+//     for (let j = i + 1; j < len; j += 1) {
+//       if (diff - numList[j] in hash) {
+//         idx.push(numList[i], numList[j], hash[diff - numList[j]]);
+//         break;
+//       }
+//       hash[numList[j]] = numList[j];
+//     }
+//   }
+//   console.log(`Three sum: ${idx[0] * idx[1] * idx[2]}`);
+//   }
+//
+// const test = [1721, 979, 366, 299, 675, 1456];
+// twoSum(data, 2020);
+// threeSum(data, 2020);
