8f552a66898906b78b3465369dcbc2d9939a33b3
talama <talama@proton.me>
2023-12-03 00:00:20 +0100

    feat: refactor solutions using regex

--- ts/2023/02/02.ts
+++ ts/2023/02/02.ts
@@ -1,64 +1,41 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-type Round = { [index: string]: number };
+type Game = Record<string, number>;
 
-type Game = {
-  id: number;
-  rounds: Round[];
-};
-
-function format(input: string): Game[] {
+function format(input: string) {
   const data = readFileSync(join(__dirname, input), 'utf-8').trim().split('\n');
-  const keys = ['red', 'green', 'blue'];
   return data.map((line) => {
-    const game = line.split(':');
-    const id = Number(game[0].slice(5));
-    const rounds = game[1].split(';').map((round) => {
-      const result: Round = {};
-      keys.forEach((key) => {
-        const index = round.indexOf(key);
-        if (index !== -1) {
-          result[key] = Number(round.slice(index - 3, index - 1));
+    const game: Game = {};
+    const rounds = line.match(/(\d+)\s(\w+)/g);
+    if (rounds) {
+      rounds.forEach((elem) => {
+        const [value, color] = elem.split(' ');
+        if (!Object.hasOwn(game, color) || game[color] < parseInt(value, 10)) {
+          game[color] = parseInt(value, 10);
         }
       });
-      return result;
-    });
-    return { id: id, rounds: rounds };
-  });
-}
-
-function solution1(data: Game[], bag: Round): number {
-  let result = 0;
-  data.forEach((game) => {
-    if (
-      !game.rounds.some((round) =>
-        Object.keys(round).some((key) => round[key] > bag[key])
-      )
-    ) {
-      result += game.id;
     }
+    return game;
   });
-  return result;
 }
 
-function solution2(data: Game[]): number {
-  let result = 0;
-  data.forEach((game) => {
-    const cubes: Round = { red: 1, green: 1, blue: 1 };
-    game.rounds.forEach((round) => {
-      Object.keys(cubes).forEach((key) => {
-        if (round[key] && round[key] > cubes[key]) {
-          cubes[key] = round[key];
-        }
-      });
-    });
-    result += cubes.red * cubes.green * cubes.blue;
+function Solutions(games: Game[], bag: Game) {
+  let sol1 = 0;
+  let sol2 = 0;
+
+  games.forEach((game, index) => {
+    const { red, blue, green } = game;
+    if (red <= bag.red && green <= bag.green && blue <= bag.blue) {
+      sol1 += index + 1;
+    }
+    sol2 += ( red * green * blue );
   });
-  return result;
+  return [sol1, sol2];
 }
 
-const data = format('input.txt');
-const bag : Round = { red: 12, green: 13, blue: 14 };
-console.log(`Solution1: ${solution1(data, bag)}`);
-console.log(`Solution2: ${solution2(data)}`);
+const bag: Game = { red: 12, green: 13, blue: 14 };
+const games = format('input.txt');
+const [sol1, sol2] = Solutions(games, bag);
+console.log(`Solution1: ${sol1}`);
+console.log(`Solution2: ${sol2}`);
