4c980fc9537f9ba0a20de04f36b8e55b8d7d4bc4
talama <talama@proton.me>
2023-12-05 18:02:15 +0100

    feat: Day 4 - Solution2

--- ts/2023/04/04.ts
+++ ts/2023/04/04.ts
@@ -24,5 +24,19 @@ function solution1(input: string[]) {
   return score.reduce((acc, curr) => acc + curr, 0);
 }
 
+function solution2 (input: string[]){
+  const copies = Array(input.length).fill(1);
+  input.forEach((line, index) => {
+    const [winners, numbers] = parseLine(line);
+    const points = numbers.filter(num => winners.includes(num));
+    // add to the next nth cards the amount of times we have the current card
+    for(let i = 1; i <= points.length; i += 1){
+      copies[index + i] += copies[index]
+    }
+  })
+  return copies.reduce((acc, curr) => acc + curr, 0);
+}
+
 const data = format('input.txt');
 console.log(`Solution1: ${solution1(data)}`);
+console.log(`Solution2: ${solution2(data)}`);
