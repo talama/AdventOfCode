d94b74f8f55963dd0cb880b0c1283791ae6484f7
talama <talama@proton.me>
2023-12-06 20:21:15 +0100

    feat: Day 6 - Solution2

--- ts/2023/06/06.ts
+++ ts/2023/06/06.ts
@@ -1,31 +1,37 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
-function format(input: string): number[][] {
+function format(input: string): string[][] {
   const data = readFileSync(join(__dirname, input), 'utf-8')
     .trim()
     .split('\n')
-    .map((line) =>
-      line
-        .split(/:\s+/)[1]
-        .split(/\s+/g)
-        .map((num) => parseInt(num, 10))
+    .map(
+      (line) => line.split(/:\s+/)[1].split(/\s+/g)
     );
   return data;
 }
 
-function solution1(times: number[], dist: number[]) {
-  return times.reduce((prev, curr, idx) => {
-    let count = 0;
-    for (let i = 1; i <= curr; i += 1) {
-      if ((curr - i) * i > dist[idx]) {
-        count += 1;
-      }
+function getWinners(time: number, dist: number) {
+  let count = 0;
+  for (let i = 1; i <= time; i += 1) {
+    if ((time - i) * i > dist) {
+      count += 1;
     }
+  }
+  return count;
+}
+
+function solution1(times: string[], dists: string[]) {
+  return times.reduce((prev, curr, idx) => {
+    const count = getWinners(parseInt(curr, 10), parseInt(dists[idx], 10));
     return prev * count;
   }, 1);
 }
 
-const [times, dist] = format('input.txt');
+const [times, dists] = format('input.txt');
 
-console.log(solution2(times, dist));
+const time = parseInt(times.join(''), 10);
+const dist = parseInt(dists.join(''), 10);
+
+console.log(`Solution1: ${solution1(times, dists)}`);
+console.log(`Solution2: ${getWinners(time, dist)}`);
