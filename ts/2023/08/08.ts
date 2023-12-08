965dd5cd794852a06ba1f3b036e49fdf170ecc53
talama <talama@proton.me>
2023-12-09 00:08:38 +0100

    feat: Day 8 - Solution2

--- ts/2023/08/08.ts
+++ ts/2023/08/08.ts
@@ -38,5 +38,35 @@ function solution1(instructions: string, map: NodeMap) {
   return steps;
 }
 
+function getSteps(instructions: string, map: NodeMap, startPos: string) {
+  let steps = 0;
+  let position = startPos;
+  while (!position.endsWith('Z')) {
+    const [left, right] = map[position];
+    const instruction = instructions.charAt(steps % instructions.length);
+    if (instruction === 'L') {
+      position = left;
+    } else {
+      position = right;
+    }
+    steps += 1;
+  }
+  return steps;
+}
+
+function gcd(a: number, b: number): number {
+  return b ? gcd(b, a % b) : a;
+}
+
+function lcm(a: number, b: number): number {
+  return (a * b) / gcd(a, b);
+}
+
 const [instructions, map] = format('input.txt');
+const startPositions = Object.keys(map).filter((key) => key.endsWith('A'));
+const steps = startPositions.map((startPos) =>
+  getSteps(instructions as string, map as NodeMap, startPos)
+);
+const solution2 = steps.reduce((a, b) => lcm(a, b));
 console.log(`Solution1: ${solution1(instructions as string, map as NodeMap)}`);
+console.log(`Solution2: ${solution2}`);
