334647589e3ca2215caddfe99ce708a16f1ff3ec
talama <talama@proton.me>
2024-12-03 00:47:59 +0100

    test(go): added tests for Day 02

---
+++ 2024/02/02_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  bool
+	}{
+		{"safeAscending", []int{1, 3, 6, 7, 9}, true},
+		{"safeDescending", []int{7, 6, 4, 2, 1}, true},
+		{"unsafeAscending", []int{1, 2, 7, 8, 9}, false},
+		{"unsafeDescending", []int{9, 7, 6, 2, 1}, false},
+		{"unsafeNoIncrease", []int{8, 6, 4, 4, 1}, false},
+		{"unsafeNotSorted", []int{1, 3, 2, 4, 5}, false},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isSafe(tt.input); got != tt.want {
+				t.Errorf("got %t, want %t", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  bool
+	}{
+		{"safeAscending", []int{1, 3, 6, 7, 9}, true},
+		{"safeDescending", []int{7, 6, 4, 2, 1}, true},
+		{"unsafeAscending", []int{1, 2, 7, 8, 9}, false},
+		{"unsafeDescending", []int{9, 7, 6, 2, 1}, false},
+		{"safeRemoved 4", []int{8, 6, 4, 4, 1}, true},
+		{"safeRemoved 3", []int{1, 3, 2, 4, 5}, true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := false
+			for i := range tt.input {
+				clone := make([]int, 0, len(tt.input)-1)
+				clone = append(clone, tt.input[:i]...)
+				clone = append(clone, tt.input[i+1:]...)
+				if isSafe(clone) {
+					got = true
+					break
+				}
+			}
+			if got != tt.want {
+				t.Errorf("got %t, want %t", got, tt.want)
+			}
+		})
+	}
+}
