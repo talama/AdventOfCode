41f03f4025c39d859473357d0a4ea41dcca8a5ee
talama <talama@proton.me>
2024-12-03 22:48:45 +0100

    test(go): added tests for Day 03 part2

--- 2024/03/03_test.go
+++ 2024/03/03_test.go
@@ -4,7 +4,10 @@ import (
 	"testing"
 )
 
-var memory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+var (
+	memory1 = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+	memory2 = " xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+)
 
 func TestPart1(t *testing.T) {
 	tests := []struct {
@@ -12,11 +15,31 @@ func TestPart1(t *testing.T) {
 		input string
 		want  []string
 	}{
-		{"memory", memory, []string{"mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"}},
+		{"memory", memory1, []string{"mul(2,4)", "mul(5,5)", "mul(11,8)", "mul(8,5)"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := parseMemory(tt.input)
+			for i, mul := range got {
+				if mul != tt.want[i] {
+					t.Errorf("got %s, want %s", mul, tt.want[i])
+				}
+			}
+		})
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  []string
+	}{
+		{"memory", memory2, []string{"mul(2,4)", "mul(8,5)"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := parseMemory(filterMemory(tt.input))
 			for i, mul := range got {
 				if mul != tt.want[i] {
 					t.Errorf("got %s, want %s", mul, tt.want[i])
