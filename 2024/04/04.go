8431769054701b0987240bbbac8cff75a7aa3652
talama <talama@proton.me>
2024-12-04 20:37:50 +0100

    feat(go): Day04 part1

--- 2024/04/04.go
+++ 2024/04/04.go
@@ -13,45 +13,45 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading file: %v", err)
 	}
-	fmt.Println(solvePuzzle(puzzle, "XMAS"))
-}
 
-func solvePuzzle(puzzle [][]string, word string) int {
-	var wordCount int
+	word1 := "XMAS"
+	var solution1 int
+
 	for rowIdx, row := range puzzle {
 		for colIdx := range row {
-			// When we find the first letter of the word we are a looking for start searching
-			if puzzle[rowIdx][colIdx] == string(word[0]) {
-				wordCount += findWord(puzzle, word, rowIdx, colIdx)
+			if puzzle[rowIdx][colIdx] == string(word1[0]) {
+				solution1 += findWord(puzzle, word1, rowIdx, colIdx)
 			}
 		}
 	}
-	return wordCount
+	fmt.Println("Solution1:", solution1)
 }
 
+// Given a [][]string and a starting point (rowIdx, rowCol), find each occurence of "word" going in all directions.
+// Return the number of occurences found.
 func findWord(puzzle [][]string, word string, rowIdx, colIdx int) int {
 	directions := [][]int{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}}
 	var wordCount int
+
 	for _, dir := range directions {
-		dr, dc := dir[0], dir[1]
 		currRow, currCol := rowIdx, colIdx
-		var found string
+		var found bool
 
 		for _, char := range word[1:] {
-
-			currRow += dr
-			currCol += dc
+			currRow += dir[0]
+			currCol += dir[1]
 
 			if currRow < 0 || currRow >= len(puzzle) || currCol < 0 || currCol >= len(puzzle[0]) {
+				found = false
 				break
 			}
-
 			if puzzle[currRow][currCol] != string(char) {
+				found = false
 				break
 			}
-			found += string(char)
+			found = true
 		}
-		if found == word[1:] {
+		if found {
 			wordCount += 1
 		}
 	}
@@ -65,11 +65,20 @@ func format(filename string) ([][]string, error) {
 		return nil, err
 	}
 
+	defer func() {
+		if err := file.Close(); err != nil {
+			log.Fatalf("Failed to close file: %v", err)
+		}
+	}()
+
 	var puzzle [][]string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		puzzle = append(puzzle, strings.Split(line, ""))
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
 	}
 	return puzzle, nil
 }
