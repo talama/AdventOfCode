2379225a24f46f1cdbb1f73714411a74500bdcc7
talama <talama@proton.me>
2024-12-10 01:52:43 +0100

    feat(go): Day09 Part1

--- 2024/09/09.go
+++ 2024/09/09.go
@@ -26,37 +26,38 @@ func main() {
 
 func getCheckSum(data string) int {
 	var head, checksum int
-	tail := len(data) - 1
-	tailVal := int(data[tail] - '0')
-
-	writeSpaces := func(spaces int) {
-		for range spaces {
-			if tailVal <= 0 {
-				tail -= 2
-				tailVal = int(data[tail] - '0')
-			}
-			checksum += head * (tail / 2)
-			head += 1
-			tailVal -= 1
-		}
-	}
+	tail := len(data) - 1           // index of the last non fully moved file
+	blocks := int(data[tail] - '0') // number of blocks remaining in the last non fully moved file
 
 	for i, r := range data {
 		val := int(r - '0')
+		// if we move past the index of the last moved file
+		// finish moving the remaining blocks if any, then exit
 		if i >= tail {
-			for range tailVal {
+			for range blocks {
 				checksum += head * (tail / 2)
 				head += 1
 			}
 			break
 		}
+		// if there is a file at the current postion write it
 		if i%2 == 0 {
 			for range val {
 				checksum += head * (i / 2)
 				head += 1
 			}
 		} else {
-			writeSpaces(val)
+			// if there is free space, write blocks from the last non fully moved file
+			// if we run out of blocks, move the tail to the next file
+			for range val {
+				if blocks <= 0 {
+					tail -= 2
+					blocks = int(data[tail] - '0')
+				}
+				checksum += head * (tail / 2)
+				head += 1
+				blocks -= 1
+			}
 		}
 	}
 	return checksum
